fd9d61690cddad774dc42e0bef3aed10450615a1
Sufiane <su.souissi@gmail.com>
2024-12-22 12:11:36 +0100

    test(admin): add test

---
+++ src/api/admin/admin.service.spec.ts
@@ -0,0 +1,83 @@
+import { AdminService } from './admin.service';
+import { Test } from '@nestjs/testing';
+import { DeepMockProxy, mockDeep } from 'jest-mock-extended';
+import { FootballDataService } from '../../football-data/football-data.service';
+import { MatchesService as MatchsDbService } from '../../db/matches/matches.service';
+import { FormattedMatch } from '../../shared/types/formatted-match.type';
+import { InternalServerErrorException } from '@nestjs/common';
+import { CreateMatchDto } from './dto/create-match.dto';
+
+describe('AdminService', () => {
+    let service: AdminService;
+    let footballDataService: DeepMockProxy<FootballDataService>;
+    let matchsDbService: DeepMockProxy<MatchsDbService>;
+
+    beforeEach(async () => {
+        const module = await Test.createTestingModule({
+            providers: [
+                AdminService,
+                {
+                    provide: FootballDataService,
+                    useValue: mockDeep<FootballDataService>(),
+                },
+                {
+                    provide: MatchsDbService,
+                    useValue: mockDeep<MatchsDbService>(),
+                },
+            ],
+        }).compile();
+
+        service = module.get(AdminService);
+        footballDataService = module.get(FootballDataService);
+        matchsDbService = module.get(MatchsDbService);
+
+        module.useLogger(false);
+    });
+
+    describe('loadMatches', () => {
+        it('should call load matches', async () => {
+            const matches = [] as FormattedMatch[];
+            footballDataService.getTeamMatches.mockResolvedValue(matches);
+
+            matchsDbService.loadMatches.mockResolvedValueOnce(undefined);
+
+            const seasonStartYear = 2022;
+
+            await expect(service.loadMatches(seasonStartYear)).resolves.toBeUndefined();
+            expect(footballDataService.getTeamMatches).toHaveBeenCalledTimes(1);
+            expect(footballDataService.getTeamMatches).toHaveBeenCalledWith(
+                524,
+                seasonStartYear,
+            );
+            expect(matchsDbService.loadMatches).toHaveBeenCalledTimes(1);
+            expect(matchsDbService.loadMatches).toHaveBeenCalledWith(matches);
+        });
+    });
+
+    describe('createMatch', () => {
+        describe('when an error occurs', () => {
+            it('should throw an internal server error', async () => {
+                matchsDbService.createMatch.mockRejectedValueOnce(new Error());
+
+                const payload = {} as CreateMatchDto;
+                await expect(service.createMatch(payload)).rejects.toThrow(
+                    InternalServerErrorException,
+                );
+                expect(matchsDbService.createMatch).toHaveBeenCalledTimes(1);
+                expect(matchsDbService.createMatch).toHaveBeenCalledWith(payload);
+            });
+        });
+
+        describe('when no error occurs', () => {
+            it('should not throw', async () => {
+                matchsDbService.createMatch.mockResolvedValueOnce(undefined);
+
+                const payload = {} as CreateMatchDto;
+
+                await expect(service.createMatch(payload)).resolves.toBeUndefined();
+                expect(matchsDbService.createMatch).toHaveBeenCalledTimes(1);
+                expect(matchsDbService.createMatch).toHaveBeenCalledWith(payload);
+            });
+        });
+    });
+});
