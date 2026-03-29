df54df41f824ab7afadc52067ba69eed24be3e1c
Sufiane <su.souissi@gmail.com>
2026-03-29 10:01:24 +0900

    fix: accounting cache keys date usage

--- src/redis/CACHE_KEYS.ts
+++ src/redis/CACHE_KEYS.ts
@@ -1,11 +1,11 @@
 export default {
     accounting: (userId: string, start: Date, end?: Date) =>
-        `accounting:user:id:${userId}:start:${start.toString()}:end:${end?.toString()}`,
+        `accounting:user:id:${userId}:start:${start.toISOString()}:end:${end?.toISOString()}`,
     invalidateAccounting: (userId: string) => `accounting:user:id:${userId}:*`,
     invalidateMatches: () => 'matches:*',
     match: (matchId: string) => `match:id:${matchId}`,
     matches: (from: Date, to?: Date) =>
-        `matches:start:${from.toString()}:end:${to?.toString()}`,
+        `matches:start:${from.toISOString()}:end:${to?.toISOString()}`,
     sale: (saleId: string) => `sale:id:${saleId}`,
     sales: (userId: string) => `user:id:${userId}:sales`,
     userByEmail: (email: string) => `user:email:${email}`,
