77eeeb194436791e1afcbfe85eb024d9ad329418
Sufiane <su.souissi@gmail.com>
2026-03-29 23:44:05 +0900

    fix: forbid negativ value on number value in dto

--- src/api/sales/dto/add-sale.dto.ts
+++ src/api/sales/dto/add-sale.dto.ts
@@ -1,16 +1,19 @@
-import { IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsInt, IsNumber, IsOptional, IsString, Min } from 'class-validator';
 
 export class AddSaleDto {
     @IsString()
     matchId: string;
 
-    @IsNumber()
+    @IsInt()
+    @Min(1)
     nbTickets: number;
 
     @IsOptional()
     @IsNumber()
+    @Min(0)
     invest: number = 0;
 
     @IsNumber()
+    @Min(1)
     listedPrice: number;
 }
